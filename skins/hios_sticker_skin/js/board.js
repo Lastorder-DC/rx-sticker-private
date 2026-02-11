4acaf72f99453a79a42dab75776fc7a84f777834
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2026-02-11 14:28:30 +0900

    Add sticker click overlay for personal block action

--- skins/hios_sticker_skin/js/board.js
+++ skins/hios_sticker_skin/js/board.js
@@ -1,4 +1,60 @@
 (function($){
+	var stickerLinkPattern = /(^|[?&])mid=sticker(&|$)/;
+	var stickerSrlPattern = /(?:^|[?&])sticker_srl=([^&#]+)/;
+
+	function getStickerInfoFromLink($link){
+		var href = $link.attr('href') || '';
+		if(!href || !stickerLinkPattern.test(href) || !stickerSrlPattern.test(href)){
+			return null;
+		}
+
+		var stickerSrl = decodeURIComponent(href.replace(stickerSrlPattern, '$1'));
+		if(!stickerSrl){
+			return null;
+		}
+
+		return {
+			href: href,
+			stickerSrl: stickerSrl
+		};
+	}
+
+	function closeStickerBlockOverlay(){
+		jQuery('.sticker-block-overlay').remove();
+		jQuery('.sticker-block-target').removeClass('sticker-block-target');
+	}
+
+	function openStickerBlockOverlay($link, stickerInfo){
+		var $img = $link.find('img').first();
+		if(!$img.length){
+			return;
+		}
+
+		closeStickerBlockOverlay();
+
+		$link.addClass('sticker-block-target');
+
+		var $overlay = jQuery('<div class="sticker-block-overlay"></div>');
+		var $blockButton = jQuery('<button type="button" class="sticker-overlay-btn sticker-overlay-btn-block" title="차단"><ion-icon name="ban-outline"></ion-icon></button>');
+		var $moveButton = jQuery('<button type="button" class="sticker-overlay-btn sticker-overlay-btn-move" title="이동"><ion-icon name="link-outline"></ion-icon></button>');
+
+		$blockButton.on('click', function(event){
+			event.preventDefault();
+			event.stopPropagation();
+			blockSticker(stickerInfo.stickerSrl);
+			closeStickerBlockOverlay();
+		});
+
+		$moveButton.on('click', function(event){
+			event.preventDefault();
+			event.stopPropagation();
+			window.open(stickerInfo.href, '_blank');
+			closeStickerBlockOverlay();
+		});
+
+		$overlay.append($blockButton).append($moveButton);
+		$link.append($overlay);
+	}
 
 	$(document).on("click", ".sticker_buy>span", function (e) {
 		var $this = $(this);
@@ -63,6 +119,26 @@
 
 	});
 
+	$(document).on('click', 'a[href*="mid=sticker"][href*="sticker_srl="]', function(e){
+		var $link = jQuery(this);
+		var stickerInfo = getStickerInfoFromLink($link);
+		if(!stickerInfo || !$link.find('img').length){
+			return;
+		}
+
+		e.preventDefault();
+		e.stopPropagation();
+		openStickerBlockOverlay($link, stickerInfo);
+	});
+
+	$(document).on('click', '.sticker-block-overlay', function(e){
+		e.stopPropagation();
+	});
+
+	$(document).on('click', function(){
+		closeStickerBlockOverlay();
+	});
+
 })(jQuery);
 
 function completeDeleteSticker(ret_obj){
@@ -125,4 +201,10 @@ function getMyPage(page){
 			alert(e.responseText);
 		}  
 	}); 
+}
+
+if(typeof window.blockSticker !== 'function'){
+	window.blockSticker = function(sticker_srl){
+		console.log('blockSticker called:', sticker_srl);
+	};
 }
